cd73ee39c85bb726f951e3302401ce1567a0bd63
zshuim <zshuim@gamil.com>
2023-12-10 15:52:13 +0800

    修正了文章实体类添加注解错误的问题

--- src/main/java/com/itheima/pojo/Article.java
+++ src/main/java/com/itheima/pojo/Article.java
@@ -2,6 +2,7 @@ package com.itheima.pojo;
 
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Null;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -26,7 +27,7 @@ public class Article {
     @URL
     private String coverImg;//封面图像
     private String state;//发布状态 已发布|草稿
-    @Null
+    @NotNull
     private Integer categoryId;//文章分类id
     private Integer createUser;//创建人ID
     private LocalDateTime createTime;//创建时间
